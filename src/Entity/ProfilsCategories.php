7c3ed9f44039d181ab3b2c48c8d14ff093a53b60
alexgrapher <briennealexandre@gmail.com>
2021-02-25 16:46:02 +0100

    entity  categories et sous_cateogries et assoc

---
+++ src/Entity/ProfilsCategories.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProfilsCategoriesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProfilsCategoriesRepository::class)
+ */
+class ProfilsCategories
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $user_id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $categorie_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(?int $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getCategorieId(): ?int
+    {
+        return $this->categorie_id;
+    }
+
+    public function setCategorieId(int $categorie_id): self
+    {
+        $this->categorie_id = $categorie_id;
+
+        return $this;
+    }
+}
