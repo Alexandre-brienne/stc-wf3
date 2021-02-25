4eaf4f1fd993ada5d91bb516277cb0c4bcfb4ab3
alexgrapher <briennealexandre@gmail.com>
2021-02-25 15:46:14 +0100

    user et entity user

---
+++ migrations/Version20210225144541.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210225144541 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD nom VARCHAR(255) NOT NULL, ADD prenom VARCHAR(255) DEFAULT NULL, ADD email VARCHAR(255) NOT NULL, ADD sexe VARCHAR(255) NOT NULL, ADD point INT DEFAULT NULL, ADD date_inscription DATETIME NOT NULL, ADD date_naissance DATE NOT NULL, ADD date_connexion DATETIME DEFAULT NULL, ADD recherche VARCHAR(255) DEFAULT NULL, ADD situation VARCHAR(255) DEFAULT NULL, ADD profession VARCHAR(255) DEFAULT NULL, ADD ville VARCHAR(255) DEFAULT NULL, ADD dapartement VARCHAR(255) DEFAULT NULL, ADD image_fond VARCHAR(255) DEFAULT NULL, ADD taille INT DEFAULT NULL, ADD cheveux VARCHAR(255) DEFAULT NULL, ADD astrologique VARCHAR(255) DEFAULT NULL, ADD amis_id INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP nom, DROP prenom, DROP email, DROP sexe, DROP point, DROP date_inscription, DROP date_naissance, DROP date_connexion, DROP recherche, DROP situation, DROP profession, DROP ville, DROP dapartement, DROP image_fond, DROP taille, DROP cheveux, DROP astrologique, DROP amis_id');
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,418 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $sexe;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $point;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_inscription;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_naissance;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $date_connexion;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $recherche;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $situation;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $profession;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $ville;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $dapartement;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image_fond;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $taille;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $cheveux;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $astrologique;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $amis_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(?string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getSexe(): ?string
+    {
+        return $this->sexe;
+    }
+
+    public function setSexe(string $sexe): self
+    {
+        $this->sexe = $sexe;
+
+        return $this;
+    }
+
+    public function getPoint(): ?int
+    {
+        return $this->point;
+    }
+
+    public function setPoint(?int $point): self
+    {
+        $this->point = $point;
+
+        return $this;
+    }
+
+    public function getDateInscription(): ?\DateTimeInterface
+    {
+        return $this->date_inscription;
+    }
+
+    public function setDateInscription(\DateTimeInterface $date_inscription): self
+    {
+        $this->date_inscription = $date_inscription;
+
+        return $this;
+    }
+
+    public function getDateNaissance(): ?\DateTimeInterface
+    {
+        return $this->date_naissance;
+    }
+
+    public function setDateNaissance(\DateTimeInterface $date_naissance): self
+    {
+        $this->date_naissance = $date_naissance;
+
+        return $this;
+    }
+
+    public function getDateConnexion(): ?\DateTimeInterface
+    {
+        return $this->date_connexion;
+    }
+
+    public function setDateConnexion(?\DateTimeInterface $date_connexion): self
+    {
+        $this->date_connexion = $date_connexion;
+
+        return $this;
+    }
+
+    public function getRecherche(): ?string
+    {
+        return $this->recherche;
+    }
+
+    public function setRecherche(?string $recherche): self
+    {
+        $this->recherche = $recherche;
+
+        return $this;
+    }
+
+    public function getSituation(): ?string
+    {
+        return $this->situation;
+    }
+
+    public function setSituation(?string $situation): self
+    {
+        $this->situation = $situation;
+
+        return $this;
+    }
+
+    public function getProfession(): ?string
+    {
+        return $this->profession;
+    }
+
+    public function setProfession(?string $profession): self
+    {
+        $this->profession = $profession;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(?string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getDapartement(): ?string
+    {
+        return $this->dapartement;
+    }
+
+    public function setDapartement(?string $dapartement): self
+    {
+        $this->dapartement = $dapartement;
+
+        return $this;
+    }
+
+    public function getImageFond(): ?string
+    {
+        return $this->image_fond;
+    }
+
+    public function setImageFond(?string $image_fond): self
+    {
+        $this->image_fond = $image_fond;
+
+        return $this;
+    }
+
+    public function getTaille(): ?int
+    {
+        return $this->taille;
+    }
+
+    public function setTaille(?int $taille): self
+    {
+        $this->taille = $taille;
+
+        return $this;
+    }
+
+    public function getCheveux(): ?string
+    {
+        return $this->cheveux;
+    }
+
+    public function setCheveux(?string $cheveux): self
+    {
+        $this->cheveux = $cheveux;
+
+        return $this;
+    }
+
+    public function getAstrologique(): ?string
+    {
+        return $this->astrologique;
+    }
+
+    public function setAstrologique(?string $astrologique): self
+    {
+        $this->astrologique = $astrologique;
+
+        return $this;
+    }
+
+    public function getAmisId(): ?int
+    {
+        return $this->amis_id;
+    }
+
+    public function setAmisId(?int $amis_id): self
+    {
+        $this->amis_id = $amis_id;
+
+        return $this;
+    }
+}
