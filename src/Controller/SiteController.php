73f028e0f52358dcfec6ae6b8641d79b99d3cb79
alexgrapher <briennealexandre@gmail.com>
2021-03-01 09:56:52 +0100

    controelr site register

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace App\Controller;
-
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Security\LoginFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+
 
 class SiteController extends AbstractController
 {
@@ -13,6 +19,58 @@ class SiteController extends AbstractController
     {
         return $this->render('site/index.html.twig', [
             'controller_name' => 'SiteController',
+        ]);
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $user->setRoles(["ROLE_USER"]);
+            $user->setSexe("homme");
+            $user->setNom("alexandre");
+            $user->setPrenom("toto");
+            // $user->setPoint(10);
+
+        
+ 
+
+
+
+
+            // $user->setAmisId(5);
+         
+            $user->setDateInscription(new \DateTime());
+            $user->setDateNaissance(new \DateTime());
+
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $authenticator,
+                'main' // firewall name in security.yaml
+            );
+        }
+
+        return $this->render('site/register.html.twig', [
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
